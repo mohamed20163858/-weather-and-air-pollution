7fedf3bc6e310f1a26e58b61e4f1f96b81ff5346
Mohamed Saleh <mohamed20163858@gmail.com>
2022-10-26 22:19:10 +0200

    Finish styling the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
-import Logo from './images/logo.png';
-
-const visitPageAction = (e) => {
-  const links = document.querySelectorAll('.outer .inner-2 ul li a');
-  for (let i = 0; i < links.length; i += 1) {
-    if (links[i] === e.target) {
-      e.target.classList.add('active');
-    } else {
-      links[i].classList.remove('active');
-    }
-  }
-};
 
 const Navbar = () => (
-  <div className="outer">
-    <div className="inner-1">
-      <NavLink to="/" className="logo">
-        <img src={Logo} alt="logo" width="40" height="40" />
-        <p>Space Travelers&apos;Hub</p>
-      </NavLink>
-    </div>
-    <div className="inner-2">
-      <ul>
-        <li><NavLink to="/" onClick={visitPageAction}>Rockets</NavLink></li>
-        <li><NavLink to="/missions" onClick={visitPageAction}>Missions</NavLink></li>
-        <li className="vertical-line" />
-        <li><NavLink to="/my-profile" onClick={visitPageAction}>My Profile</NavLink></li>
-      </ul>
+  <div className="navbar">
+    <NavLink to="/" className="logo">
+      <i className="fa-solid fa-house-user" />
+    </NavLink>
+    <p>Weather and Air Pollution</p>
+    <div className="left">
+      <i className="fa-solid fa-microphone" />
+      <i className="fa-solid fa-gear" />
     </div>
   </div>
 );
