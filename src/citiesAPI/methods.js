61f7ce026be51cafda50b70ed6515ff8f9097a65
Mohamed Saleh <mohamed20163858@gmail.com>
2022-10-28 22:27:08 +0200

    Use https instead of http for all apis

--- src/citiesAPI/methods.js
+++ src/citiesAPI/methods.js
@@ -2,7 +2,7 @@ const fetchAllCaptialCities = () => fetch('https://countriesnow.space/api/v0.1/c
 const fetchAllCountryCities = () => fetch('https://countriesnow.space/api/v0.1/countries');
 const fetchALLCountriesFlags = () => fetch('https://countriesnow.space/api/v0.1/countries/flag/images');
 const apiKey = 'f43d5eb45dbe323f3208c22e02d05c24';
-const fetchCityGeocoding = (city, iso2) => fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${iso2}}&appid=${apiKey}`);
+const fetchCityGeocoding = (city, iso2) => fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},${iso2}}&appid=${apiKey}`);
 const fetchCityWeather = (lat, lon) => fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
 const fetchCityAirPollution = (lat, lon) => fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
 
